518ad1bd855825c59236ad3c080e09f6edaace32
Rohit <rohitdeshmukh69@gmail.com>
2014-04-29 22:24:45 +0100

    added method get_author_book_list_by_id

--- lib/WebService/Goodreads/Role/Author.pm
+++ lib/WebService/Goodreads/Role/Author.pm
@@ -10,10 +10,20 @@ has author_uri =>(
     isa =>  'Str',
     default => 'author/show'
 );
+has author_book_list_uri =>(
+    is  =>  'ro',
+    isa =>  'Str',
+    default => 'author/list'
+);
 sub get_author_by_id{
     my ($self,$rh_args) = @_;
     croak "author id is required" unless($rh_args->{id});
     return  $self->ua->get(uri_encode($self->base_url.$self->author_uri.'.'.$self->format.'?id='.$rh_args->{'id'}.'&key='.$self->key));
 }
 
+sub get_author_book_list_by_id{
+    my ($self,$rh_args) = @_;
+    croak "author id is required" unless($rh_args->{id});
+    return  $self->ua->get(uri_encode($self->base_url.$self->author_book_list_uri.'.'.$self->format.'?id='.$rh_args->{'id'}.'&key='.$self->key.( $rh_args->{page} ?'&page='.$rh_args->{page}: '') ));
+}
 1;
