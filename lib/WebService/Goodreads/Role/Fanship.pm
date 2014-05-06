b57b049dac362fadcf1cf96b97c636b88a5da848
Rohit <rohitdeshmukh69@gmail.com>
2014-05-06 21:34:29 +0100

    added Fanship Role for books

---
+++ lib/WebService/Goodreads/Role/Fanship.pm
@@ -0,0 +1,12 @@
+package WebService::Goodreads::Role::Fanship;
+use Moose::Role;
+use Modern::Perl;
+use namespace::autoclean;
+use URI::Encode qw(uri_encode);
+use Carp;
+use Data::Dumper;
+
+
+
+
+1;
